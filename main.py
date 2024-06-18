d3b3312a329980c7cad3f7cbc69f4c54daafd42c
Davvos11 <vosdavid2@gmail.com>
2024-06-18 14:38:30 +0200

    add opencv transparency spaghett

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import json
 import os
-import urllib.request, json
+import urllib.request
 from typing import List
 
+import cv2
+import numpy as np
 import requests
+from PIL import Image
 
 KEY = os.getenv('NS_KEY')
 
@@ -41,6 +45,36 @@ def get_train_info_for_ids(ids: List[int]):
     return json.loads(response.read().decode('utf-8'))
 
 
+def make_transparent(image_path: str):
+    img = cv2.imread(image_path)
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img_rgba = cv2.cvtColor(img, cv2.COLOR_BGR2BGRA)
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    _, thresh = cv2.threshold(gray, 240, 255, cv2.THRESH_BINARY)
+
+    # Find contours
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Create mask
+    mask = np.zeros_like(gray)
+    cv2.drawContours(mask, contours, -1, (255), thickness=cv2.FILLED)
+
+    # Apply mask to the alpha channel
+    alpha_channel = np.ones_like(mask) * 255
+    alpha_channel[mask == 255] = 0
+
+    # Set alpha channel
+    img_rgba[:, :, 3] = alpha_channel
+    img_rgba[:, :, 2] = img_rgb[:, :, 2]
+    img_rgba[:, :, 1] = img_rgb[:, :, 1]
+    img_rgba[:, :, 0] = img_rgb[:, :, 0]
+
+    # Save the image with transparency
+    output_image = Image.fromarray(img_rgba)
+    return output_image
+
+
 def download_img(url: str, name: str):
     response = requests.get(url)
     if response.status_code == 200:
@@ -48,6 +82,13 @@ def download_img(url: str, name: str):
         location = f"./out/{name.replace('/', '_')}"
         with open(location, 'wb') as file:
             file.write(response.content)
+        # Check for transparency
+        img = Image.open(location)
+        if img.mode == 'RGBA' and img.getchannel('A').getextrema() != (255, 255):
+            pass
+        else:
+            img = make_transparent(location)
+            img.save(location, 'png')
     else:
         print(f"Failed to download {url}")
 
